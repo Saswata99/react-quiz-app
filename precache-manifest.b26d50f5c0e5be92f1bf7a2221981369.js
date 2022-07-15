4498e6abcd7e8da06fa0d9763d950b869b338d80
saswatax <75081293+Saswatax@users.noreply.github.com>
2022-07-15 12:24:52 +0530

    Updates

---
+++ precache-manifest.b26d50f5c0e5be92f1bf7a2221981369.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c6fc8ae76f42ed6c9e0",
+    "url": "/react-quiz-app/static/css/main.95c9245a.chunk.css"
+  },
+  {
+    "revision": "0c6fc8ae76f42ed6c9e0",
+    "url": "/react-quiz-app/static/js/main.0c6fc8ae.chunk.js"
+  },
+  {
+    "revision": "87f44c120abf48e30b68",
+    "url": "/react-quiz-app/static/js/1.87f44c12.chunk.js"
+  },
+  {
+    "revision": "6cfabd6c4f363fd18b01",
+    "url": "/react-quiz-app/static/js/runtime~main.6cfabd6c.js"
+  },
+  {
+    "revision": "ca3b71e5f6022a396991cd5ceadc9e88",
+    "url": "/react-quiz-app/index.html"
+  }
+];
